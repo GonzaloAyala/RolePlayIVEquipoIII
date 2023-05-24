e9261db36ff7f0515a7b3866d1237f0ff8111ddc
fedepds <federico.pizarro@correo.ucu.edu.uy>
2023-05-24 20:24:54 -0300

    cree clase Sling

---
+++ src/Library/items/Sling.cs
@@ -0,0 +1,21 @@
+namespace RoleplayGame.Items
+{
+    /// <summary>
+    /// Sling. Permite atacar.
+    /// </summary>
+
+    public class Sling : IAttackItem
+    {
+        /// <summary>
+        /// El poder de ataque
+        /// </summary>
+        /// <value></value>
+        public int AttackPower
+        {
+            get
+            {
+                return 20;
+            }
+        }
+    }
+}
