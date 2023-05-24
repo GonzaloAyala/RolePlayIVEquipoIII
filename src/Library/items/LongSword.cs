38cc1cae0138b6a4c53a3d80dafafa7f2ccfc0fd
Cristianbaumann <Cristian.baumann@correo.ucu.edu.uy>
2023-05-24 20:26:30 -0300

    New item LongSword

---
+++ src/Library/items/LongSword.cs
@@ -0,0 +1,13 @@
+namespace RoleplayGame.Items
+{
+    public class LongSword: IAttackItem
+    {
+        public int AttackPower 
+        {
+            get
+            {
+                return  25;
+            } 
+        }
+    }
+}
