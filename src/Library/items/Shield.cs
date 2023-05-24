2aaee05607e7089509544d573b060dfd86d5ea38
Cristianbaumann <Cristian.baumann@correo.ucu.edu.uy>
2023-05-24 20:38:20 -0300

    New Shield

---
+++ src/Library/items/Shield.cs
@@ -0,0 +1,17 @@
+namespace RoleplayGame.Items
+{
+    public class Shield: IDefenseItem
+    {
+        public int DefensePower 
+        {
+            get
+            {
+                return  30;
+            } 
+        }
+        public override string ToString()
+        {
+            return "Tower Shield";
+        }
+    }
+}
